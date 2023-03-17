aceadbe9d0c219fcb72affa5e3cdcd7536cdf677
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2023-03-17 10:56:52 +0300

    crud operations complete

--- express-js/server.js
+++ express-js/server.js
@@ -4,22 +4,12 @@ const path = require("path");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
 const PORT = process.env.port || 4000;
 //custom logger middleware
 app.use(logger);
 //apply cors cross-origin-resource-sharing
-const whitelist = ["https://www.codereis.com", "https://codereis.com"];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("not allowed by cors"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
 app.use(cors(corsOptions));
 //middleware is to handle form data
 app.use(express.urlencoded({ extended: false }));
