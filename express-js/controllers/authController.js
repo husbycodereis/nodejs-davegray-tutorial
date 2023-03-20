fd574144175240f287cf3ced35c50724c6900c41
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2023-03-20 10:27:13 +0300

    adding jwt token logic

--- express-js/controllers/authController.js
+++ express-js/controllers/authController.js
@@ -8,6 +8,9 @@ const fsPromises = require("fs").promises;
 const path = require("path");
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const handleLogin = async (req, res) => {
   const { user, password } = req.body;
   if (!user || !password) {
@@ -15,13 +18,39 @@ const handleLogin = async (req, res) => {
   }
   const foundUser = usersDB.users.find((u) => u.username === user);
   if (!foundUser) {
-    return res.sendStatus(401).json({ message: "no user found" }); //unauthorized
+    return res.sendStatus(409).json({ message: "no user found" }); //unauthorized
   }
   //evaluate password
   const match = await bcrypt.compare(password, foundUser.password);
   if (match) {
     //create JWT to use with other routes
-    return res.json({ success: `User ${user} is logged in` });
+    console.log(process.env.ACCESS_TOKEN_SECRET);
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    //Saving refresh token with current user
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000, //one day in miliseconds
+    });
+    return res.json({ accessToken, refreshToken });
   } else {
     return res.sendStatus(401);
   }
